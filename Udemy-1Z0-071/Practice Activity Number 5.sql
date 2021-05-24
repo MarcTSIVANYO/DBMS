567671a380bcaa6fb9260af2bd5eb33b409db8c3
Marc TSIVANYO <marctsivanyo@gmail.com>
2021-05-24 10:03:50 +0200

    Add patiques files

---
+++ Udemy-1Z0-071/Practice Activity Number 5.sql
@@ -0,0 +1,6 @@
+SELECT Q'(' The first person is called Simon')' AS Result FROM DUAL;
+SELECT ' The first person is called Simon' AS Result FROM DUAL;
+
+SELECT SUBSTR(' The first person is called Simon',5,6) || ' second' AS Result FROM DUAL;
+
+SELECT TRIM(' The first person is called Simon') AS Result FROM DUAL;
